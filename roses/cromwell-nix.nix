0e06af2df042c6d9d1af52c8d48dd0270b0db0f9
Charles Eckman <charles@cceckman.com>
2022-11-27 17:13:15 -0500

    Add missing nixos qualifier to pkgs name

--- roses/cromwell-nix.nix
+++ roses/cromwell-nix.nix
@@ -16,7 +16,7 @@
   };
 
   # Enable nix flakes
-  nix.package = pkgs.nixFlakes;
+  nix.package = nixos.pkgs.nixFlakes;
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
