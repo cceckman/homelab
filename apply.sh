b8afdcc8a6559377a077fd51d97114bb24ec2c9f
Charles Eckman <charles@cceckman.com>
2022-12-10 19:15:12 +0000

    Update apply to support multiple hosts

--- apply.sh
+++ apply.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-sudo nixos-rebuild switch --flake .
+# Usage: ./apply.sh [ <hostame> ]
+
+if test -n "$1"
+then
+  nixos-rebuild switch --use-remote-sudo --target-host $1 --flake .'#'$1
+else
+  sudo nixos-rebuild switch --flake .
+fi
+
