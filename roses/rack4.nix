4109e8d3c7c711f291793be2543e2f0b52c4c442
Charles Eckman <charles@cceckman.com>
2022-12-14 20:01:50 +0000

    Start backups via restic
    Turns out most of what I wanted to do - "regularly scheduled backups,
    use blobstore rather than disk/file for cost efficiencies" - is covered
    by restic.
    
    Leaving off the timer for now, until the first backup completes.

--- roses/rack4.nix
+++ roses/rack4.nix
@@ -13,7 +13,7 @@
 
   # Support for external media drive
   boot.supportedFilesystems = [ "ntfs" ];
-  users.groups.mediahd-access.members = [ "navidrome" "cceckman" ];
+  users.groups.mediahd-access.members = [ "navidrome" "cceckman" "restic" ];
   fileSystems."/media/mediahd" = {
     device = "/dev/disk/by-uuid/5CA43549A4352744";
     options = [ "rw" "noatime" "users" "nofail" "x-systemd.mount-timeout=5s" ];
@@ -26,4 +26,31 @@
       authKeyPath = "/var/secrets/navidrome-proxy-authkey.txt";
     }
   ];
+
+  # Backup the external media drive
+  users.users.restic = {
+    isSystemUser = true;
+    description  = "Restic backups user";
+    group = "restic";
+  };
+  users.groups.restic = {};
+
+  services.restic = {
+    backups.remote = {
+      passwordFile = "/etc/nixos/secrets/restic/password";
+      environmentFile = "/etc/nixos/secrets/restic/environment";
+      paths = [
+        "/media/mediahd"
+      ];
+      initialize = true;
+      repository = "gs:xueckman-backup:restic/";
+      user = "restic";
+
+     #timerConfig = {
+     #  OnCalendar = "03:05";
+     #  RandomizedDelaySec = "2h";
+     #};
+    };
+
+  };
 }
