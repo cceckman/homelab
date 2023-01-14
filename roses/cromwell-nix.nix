4646a89b50bf13ebd8bb74c66c0d91f04c528654
Charles Eckman <charles@cceckman.com>
2023-01-14 09:25:49 -0500

    Mount bigdata directly in WSL
    More comfortable with doing file copies with rsync...

--- roses/cromwell-nix.nix
+++ roses/cromwell-nix.nix
@@ -17,4 +17,18 @@
 
   # Enable crossbuild:
   boot.binfmt.emulatedSystems = [ "aarch64-linux" "armv7l-linux" ];
+
+  # Mount bigdata
+  environment.systemPackages = [ pkgs.cifs-utils ];
+  fileSystems."/mnt/bigdata" = {
+    device = "//rack4.monkey-heptatonic.ts.net/bigdata";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      user_opts = "uid=1000,gid=100";
+      auth_opts = "guest";
+    in ["${automount_opts},${user_opts},${auth_opts}"];
+  };
+
 }
