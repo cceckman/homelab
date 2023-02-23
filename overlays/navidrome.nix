ed92595028826ed275ba528dd13c953f708f66a0
Charles Eckman <charles@cceckman.com>
2023-02-23 08:47:42 -0500

    Build navidrome at baseline, for reference

--- overlays/navidrome.nix
+++ overlays/navidrome.nix
@@ -2,7 +2,8 @@ self: super: {
   # All sorts of fun:
   # https://discourse.nixos.org/t/inconsistent-vendoring-in-buildgomodule-when-overriding-source/9225
   navidrome = let
-    overrideVersion = "0.49.3-dev.6";
+   #overrideVersion = "0.49.3-dev.8";
+    overrideVersion = "0.48.0";
   in super.buildGoModule {
     inherit (super.navidrome.drvAttrs)
       pname doCheck nativeBuildInputs buildInputs
@@ -12,12 +13,13 @@ self: super: {
     version = overrideVersion;
 
     src = super.fetchFromGitHub {
-      owner = "cceckman";
+      owner = "navidrome";
       repo = "navidrome";
       rev = "v${overrideVersion}";
-      hash = "sha256-ayquHx6axtUT/QZOvpHp9Jjbud0HEY2/xXxR91sMy4g=";
+      hash = "sha256-FO2Vl3LeajvZ8CLtnsOSLXr//gaOWPbMthj70RHxp+Q=";
     };
 
-    vendorSha256 = "sha256-t9R3qGXrZV1AF5epCjh2wi/uTf8g/kyvmaxQFj7xeLc=";
+    # vendorSha256 = "sha256-t9R3qGXrZV1AF5epCjh2wi/uTf8g/kyvmaxQFj7xeLc=";
+    vendorSha256 = "sha256-LPoM5RFHfTTWZtlxc59hly12zzrY8wjXGZ6xW2teOFM=";
   };
 }
