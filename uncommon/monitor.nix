8042d473fb6bd4640bf772a3969ad640292d5784
Charles Eckman <charles@cceckman.com>
2023-02-23 19:30:15 -0500

    Limit Prometheus memory usage

--- uncommon/monitor.nix
+++ uncommon/monitor.nix
@@ -21,37 +21,42 @@ in {
     enable = true;
     stateDir = promStateInVarLib;
     port = promPort;
-    scrapeConfigs = [ {
-      job_name = "node_exporter";
-      dns_sd_configs = [{
-        names = builtins.map (x: x + ".monkey-heptatonic.ts.net") [
-          "rack3"
-          "rack4"
-          "rack11"
+    # Limit memory usage to a cap:
+    extraFlags = [
+      "-storage.local.target-heap-size=${builtins.toString ( 128 * 1024 * 1024 )}"
+    ];
+    scrapeConfigs = [
+      {
+        job_name = "node_exporter";
+        dns_sd_configs = [{
+          names = builtins.map (x: x + ".monkey-heptatonic.ts.net") [
+            "rack3"
+            "rack4"
+            "rack11"
+          ];
+          port = 9100;
+          type = "A";
+        }];
+        relabel_configs = [
+          {
+            source_labels = ["__address__"];
+            target_label = "address";
+          }
+          {
+            source_labels = ["__meta_dns_name"];
+            target_label = "node";
+          }
         ];
-        port = 9100;
-        type = "A";
-      }];
-      relabel_configs = [
-        {
-          source_labels = ["__address__"];
-          target_label = "address";
-        }
-        {
-          source_labels = ["__meta_dns_name"];
-          target_label = "node";
-        }
-      ];
-    }
-    {
-      job_name = "prometheus";
-      static_configs = [{ targets = ["localhost:${toString promPort}"]; }];
-    }
-    {
-      job_name = "navidrome";
-      static_configs = [{ targets = [ "navidrome.monkey-heptatonic.ts.net" ]; }];
-    }
-  ];
+      }
+      {
+        job_name = "prometheus";
+        static_configs = [{ targets = ["localhost:${toString promPort}"]; }];
+      }
+      {
+        job_name = "navidrome";
+        static_configs = [{ targets = [ "navidrome.monkey-heptatonic.ts.net" ]; }];
+      }
+    ];
     remoteWrite = [{
       url = "https://prometheus-us-central1.grafana.net/api/prom/push";
       basic_auth = {
