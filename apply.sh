446a98fe2c8d36a7c40bf68b78b3ca4535004566
Charles Eckman <charles@cceckman.com>
2023-03-25 10:33:34 -0400

    Allow flag for building on remote
    Doesn't always work.

--- apply.sh
+++ apply.sh
@@ -1,10 +1,32 @@
 #!/bin/sh
 
-# Usage: ./apply.sh [ <hostame> ]
+# Usage: ./apply.sh [ --build-there | -r ] [ <hostame> ]
 
-if test -n "$1"
+TARGET=""
+REMOTE_BUILD="false"
+for arg in "$@"
+do
+  case "$arg" in
+    "--build-there"|"-r") REMOTE_BUILD="true";;
+    "-"$) echo >&2 "Unrecognized flag: $arg"; exit 1;;
+    *) TARGET="$arg";;
+  esac
+done
+
+BUILD_ARGS=""
+if "$REMOTE_BUILD"
 then
-  nixos-rebuild switch --build-host ${1}.ts --use-remote-sudo --target-host ${1}.ts --flake .'#'$1
+  BUILD_ARGS="--build-host ${TARGET.ts}"
+fi
+
+
+if test -n "$TARGET"
+then
+  nixos-rebuild switch \
+    $BUILD_ARGS \
+    --use-remote-sudo \
+    --target-host ${1}.ts \
+    --flake .'#'$TARGET
 else
   sudo nixos-rebuild switch --flake .
 fi
