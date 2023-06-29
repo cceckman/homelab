cc1a47b2cdbe0dec1f9f6f8fa1e19f0e7e37d2e9
Charles Eckman <charles@cceckman.com>
2023-06-28 22:21:14 -0400

    Fix yaml link, force curl to fail

--- debbie.sh
+++ debbie.sh
@@ -2,8 +2,8 @@
 # Set up a Debian / Ubuntu machine to my liking, via Ansible.
 
 sudo apt install -y ansible curl
-curl -Lo bootstrap.yaml \
-  https://raw.githubusercontent.com/cceckman/homelab/tree/ansible/debbootstrap.yaml
+curl --fail -Lo debootstrap.yaml \
+  https://raw.githubusercontent.com/cceckman/homelab/ansible/debootstrap.yaml
 
 sudo ansible-playbook debootstrap.yaml
 
