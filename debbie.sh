d6bfa98a13435cd67800ebce5536edfb19a9db69
Charles Eckman <charles@cceckman.com>
2023-07-06 18:40:58 -0400

    Set an option that allows 'full' clones
    This is "desktop" mode...but ultimately I need to figure out who is
    going to be a submodule of who, and avoid this option.

--- debbie.sh
+++ debbie.sh
@@ -1,6 +1,11 @@
 #! /bin/bash -eu
 # Set up a Debian / Ubuntu machine to my liking, via Ansible.
 
+if test "$1" = "--full"
+then
+  VARS="-e full_home"
+fi
+
 sudo apt install -y ansible curl
 curl --fail -Lo debootstrap.yaml \
   https://raw.githubusercontent.com/cceckman/homelab/ansible/debootstrap.yaml
@@ -11,5 +16,5 @@ do
   echo >&2 "Retry?"
 done
 # Run this as the current user; "become" only as needed
-ansible-playbook debootstrap.yaml
+ansible-playbook $VARS debootstrap.yaml
 
