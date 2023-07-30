fb03c3d65e296a90a448b6069a472e8c332d2803
Charles Eckman <charles@cceckman.com>
2023-07-29 22:46:04 -0400

    Install ipmitool on rack19; forward proxmox too

--- scripts/forward-admin.sh
+++ scripts/forward-admin.sh
@@ -1,8 +1,12 @@
 #! /bin/sh
 #
 # Connect to rack18 (BMC bounce host) via SSH, and forward the web interface.
-echo >&2 "Starting local port forwarding from :8192 to BMC HTTPS interface..."
+echo >&2 "Starting local port forwarding..."
+echo >&2 "  :8192 BMCWeb"
+echo >&2 "  :8006 Proxmox console"
+
 ssh -N -v \
   -L 8192:192.168.3.190:443 \
+  -L 8006:192.168.2.191:8006 \
   rack18
 
