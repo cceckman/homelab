45f8af5510b0d126f773d9a557a79c16128e7603
Charles Eckman <charles@cceckman.com>
2023-08-12 08:06:37 -0400

    Deploy node_exporter

--- scripts/forward-admin.sh
+++ scripts/forward-admin.sh
@@ -4,9 +4,11 @@
 echo >&2 "Starting local port forwarding..."
 echo >&2 "  :8192 BMCWeb"
 echo >&2 "  :8006 Proxmox console"
+echo >&2 "  :9119 Node exporter (rack19)"
 
-ssh -v \
+ssh \
   -L 8192:192.168.3.190:443 \
   -L 8006:192.168.2.191:8006 \
+  -L 9119:192.168.2.191:9100 \
   rack18 -- "sh -c './ipmitool power on ; read NONE'"
 
