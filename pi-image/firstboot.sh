85eefd41aac3f5d83bef6fa3972429f46c894520
Charles Eckman <charles@cceckman.com>
2025-05-29 21:30:18 -0400

    Misc updates, pi-oriented

--- pi-image/firstboot.sh
+++ pi-image/firstboot.sh
@@ -3,36 +3,65 @@
 set -eu
 # First-boot setup that Raspbian doesn't like to let us do itself.
 
-raspi-config nonint do_change_timezone "America/New_York"
-raspi-config nonint do_change_locale "en_US.UTF-8 UTF-8"
-raspi-config nonint do_wifi_country "US"
-# 0 is enable!
-raspi-config nonint do_i2c 0
-raspi-config nonint do_ssh 0
+log() {
+  echo >&2 "$@"
+}
 
-while ! test -d /home/cceckman/
-do
-  sleep 30
-done
+rpi_config() {
+  raspi-config nonint do_change_timezone "America/New_York"
+  raspi-config nonint do_change_locale "en_US.UTF-8 UTF-8"
+  raspi-config nonint do_wifi_country "US"
+  # 0 is enable!
+  raspi-config nonint do_i2c 0
+  raspi-config nonint do_ssh 0
+}
 
-su cceckman -c "if ! test -d ~/.ssh; then mkdir -m 0700 ~/.ssh; fi"
-su cceckman -c "touch ~/.ssh/authorized_keys"
-su cceckman -c "chmod 0755 ~/.ssh/authorized_keys"
-su cceckman -c "echo 'sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIFhsrsc3V3KcH79keRp/jL38ty/BHh5897avu2hMvthJAAAABHNzaDo= cceckman@cromwell' >~/.ssh/authorized_keys"
+wait_homedir() {
+  while ! test -d /home/cceckman/
+  do
+    sleep 30
+  done
+}
 
-# Set up WiFi before having anything to do with apt
-/opt/firstboot-wifi.sh
+load_key() {
+  su cceckman -c "if ! test -d ~/.ssh; then mkdir -m 0700 ~/.ssh; fi"
+  su cceckman -c "touch ~/.ssh/authorized_keys"
+  su cceckman -c "chmod 0755 ~/.ssh/authorized_keys"
+  su cceckman -c "echo 'sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIFhsrsc3V3KcH79keRp/jL38ty/BHh5897avu2hMvthJAAAABHNzaDo= cceckman@cromwell' >~/.ssh/authorized_keys"
+}
 
-# Recommended in
-# https://github.com/hzeller/rpi-rgb-led-matrix/tree/master?tab=readme-ov-file#use-minimal-raspbian-distribution
-apt-get remove -y bluez bluez-firmware pi-bluetooth triggerhappy pigpio
+install_wifi() {
+  # Set up WiFi before having anything to do with apt
+  /opt/firstboot-wifi.sh
+}
 
-# Tailscale setup, up-front:
-curl -fsSL https://tailscale.com/install.sh | sh
+remove_packages() {
+  # Recommended in
+  # https://github.com/hzeller/rpi-rgb-led-matrix/tree/master?tab=readme-ov-file#use-minimal-raspbian-distribution
+  apt-get remove -y bluez bluez-firmware pi-bluetooth triggerhappy pigpio
+}
 
-if test -f /etc/tskey
-then
-  tailscale up --auth-key=$(cat /etc/tskey) && rm /etc/tskey
-fi
+setup_tailscale() {
+  # Tailscale setup, up-front:
+  curl -fsSL https://tailscale.com/install.sh | sh
 
-systemctl disable cce-firstboot
+  if test -f /etc/tskey
+  then
+    tailscale up --auth-key=$(cat /etc/tskey) && rm /etc/tskey
+  fi
+}
+
+stop_self() {
+  systemctl disable cce-firstboot
+}
+
+main() {
+  for f in rpi_config wait_homedir load_key install_wifi remove_packages setup_tailscale stop_self
+  do
+    log "Starting $f..."
+    $f
+    log "Completed $f"
+  done
+}
+
+{ main && exit 0 }
